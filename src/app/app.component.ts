1ba046d331d0bfe145bc6d4ffde8db77c8bd2343
SamuelAispuro <saaw2612000@gmail.com>
2023-05-29 12:58:00 -0700

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Employee } from './models/employee';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+  employeeArray: Employee[] = [
+    {id: 1, name: "Samuel", country: "México"},
+    {id: 2, name: "Ryan", country: "USA"},
+    {id: 3, name: "Josue", country: "Honduras"}
+  ];
 }
